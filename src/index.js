765af57da2f3000438403dff688b592b3bbc2779
katekosushkina <kate.kosushkina@gmail.com>
2018-09-30 13:26:08 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  let res = 0;
+  let minRes = 999999999;
+  let div = 2;
+  let countDiv = 0;
+  let mNumber = number;
+  while (base>1){
+    while (base%div == 0){
+      countDiv++;
+      base = Math.floor(base/div);
+    }
+    if (countDiv !=0){
+      res = 0;
+      while (mNumber > 0){
+        mNumber = Math.floor(mNumber/div);
+        res = res + mNumber;
+      }
+      res = Math.floor(res/countDiv);
+      if (res<minRes) minRes=res;
+    }
+    div = div+1;
+    countDiv = 0;
+    mNumber = number;
+  }
+  return minRes;
 }
